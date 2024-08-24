f06cdb858e45df26d80ee96a69c4c38ac74bbfec
hoit1011 <parksm071011@gmail.com>
2024-08-24 14:55:42 +0900

    Initial Commit

---
+++ GSwiki/GSwikiApp.swift
@@ -0,0 +1,17 @@
+//
+//  GSwikiApp.swift
+//  GSwiki
+//
+//  Created by 박성민 on 8/24/24.
+//
+
+import SwiftUI
+
+@main
+struct GSwikiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
